1076fd96af67ba5654175992831a688ff89671ed
Pandu Rao <pandu-rao@socketek.com>
2015-05-24 00:11:56 -0700

    Added java, maven and storm to jump

--- 00-master-bootstrap.sh
+++ 00-master-bootstrap.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-add-apt-repository -y ppa:saltstack/salt
-apt-get -yqq update && apt-get -yqq upgrade
-apt-get -yqq install mg git nmap
-apt-get -yqq install gcc g++
-apt-get -yqq install python-dev virtualenvwrapper
+add-apt-repository -y ppa:saltstack/salt;
+apt-add-repository -y ppa:webupd8team/java;
+apt-get -yqq update && apt-get -yqq upgrade;
+apt-get -yqq install mg git nmap;
+apt-get -yqq install gcc g++;
+apt-get -yqq install python-dev virtualenvwrapper;
+apt-get install oracle-java7-installer;
 
-apt-get -yqq remove python-pip
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-python get-pip.py
-ln -s /usr/local/bin/pip /usr/bin/pip
+apt-get -yqq remove python-pip;
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py;
+python get-pip.py;
+ln -s /usr/local/bin/pip /usr/bin/pip;
 
-apt-get -yqq install salt-cloud
-apt-get -yqq install salt-master salt-minion
+murl='http://www.gtlib.gatech.edu/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz';
+surl='http://psg.mtu.edu/pub/apache/storm/apache-storm-0.9.4/apache-storm-0.9.4.tar.gz';
+mkdir ~/maven && wget "$murl" -O - | tar zxvf - -C maven --strip-components=1;
+mkdir ~/storm && wget "$surl" -O - | tar zxvf - -C storm --strip-components=1;
+echo 'export PATH=$PATH:~/maven/bin:~/storm/bin' >> ~/.bashrc;
+
+apt-get -yqq install salt-cloud;
+apt-get -yqq install salt-master salt-minion;
 
 # Clone repo
 su ubuntu <<'EOF'
